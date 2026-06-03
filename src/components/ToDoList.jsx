fe9c7a0aaa6d7180c41a1cc22068b8308866b4cd
Yash Arjitwar <yashsmac@Yashs-MacBook-Air.local>
2026-06-03 14:00:31 +0530

    Add empty list message and final polish

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -2,13 +2,23 @@
 
 import ToDoItem from "./ToDoItem";
 
-function ToDoList({ todos, deleteTodo }) {
+function ToDoList({ todos, deleteTodo, toggleComplete, editTodo }) {
+  if (todos.length === 0) {
+    return <p className="empty-message">No tasks yet. Add one above!</p>;
+  }
   return (
     <div className="todo-list">
       {todos.map(todo => (
-        <ToDoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} />
+        <ToDoItem
+          key={todo.id}
+          todo={todo}
+          deleteTodo={deleteTodo}
+          toggleComplete={toggleComplete}
+          editTodo={editTodo}
+        />
       ))}
     </div>
   );
 }
+
 export default ToDoList;
